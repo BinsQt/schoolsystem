f1e8cadc2ff4dea6b04867984bc3faaf7e1b6ce5
BinsQt <vincezamora007@gmail.com>
2024-04-23 23:41:18 +0800

    update public page

--- frontend/public/partials/main-public.php
+++ frontend/public/partials/main-public.php
@@ -1,9 +1,28 @@
 <div class="content">
     <div class="card-ad">
-
     </div>
 
-    <div class="enroll">
-        ssdfs
+
+
+<div class="enroll">
+    <div class="top">
+        <div class="header-enroll">
+            <p>School Admission</p>
+        </div>
+        <div>
+            <svg width='260' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="4" d="M0,32L40,74.7C80,117,160,203,240,224C320,245,400,203,480,160C560,117,640,75,720,74.7C800,75,880,117,960,133.3C1040,149,1120,139,1200,112C1280,85,1360,43,1400,21.3L1440,0L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path></svg> 
+        </div>
+    </div>
+
+    <div class="bottom">
+        <div>
+            <svg width='260' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="4" d="M0,32L40,74.7C80,117,160,203,240,224C320,245,400,203,480,160C560,117,640,75,720,74.7C800,75,880,117,960,133.3C1040,149,1120,139,1200,112C1280,85,1360,43,1400,21.3L1440,0L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+        </div>
+        <div class="footer-enroll">
+            <div>
+                <button>Enroll Now</button>
+            </div>
+        </div>
     </div>
 </div>
+</div>  
